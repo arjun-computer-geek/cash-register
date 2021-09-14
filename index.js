102ac20accd94cc7e90dff0d351723e60d175046
Arjun <arjun2000raj@gmail.com>
2021-09-15 03:34:59 +0530

    Cash register finish

---
+++ index.js
@@ -0,0 +1,56 @@
+const checkBtn = document.getElementById('check-btn');
+const errorfield = document.querySelector('.error');
+const notefield = document.querySelector('#note-field');
+const notes = [2000, 500, 200, 100, 50, 20, 10, 5, 2, 1]
+
+
+//showing error to error box
+const showError = (message) => {
+    errorfield.innerHTML = message
+}
+
+const printNumberOfNotes = (numOfNotes) => {
+    
+        notefield.innerHTML += `<td> ${numOfNotes} </td>`;
+}
+
+//calculating the number of notes
+const calculateNotes = (amount) => {
+
+    //traversing through notes
+    notes.forEach( note => {
+        
+        // Finding number of notes
+        const numOfNotes = Math.trunc(amount / note);
+
+        //calculating remaining amount
+        amount = amount % note;
+
+        //printing number of notes in table-data field
+        printNumberOfNotes(numOfNotes)
+    })
+}
+
+//Validating the bill amounts
+const validateAmounts = () => {
+    const billAmount = parseInt(document.querySelector('#bill-amount').value);
+    const cashGiven = parseInt(document.getElementById('cash-given').value);
+
+    if(billAmount > 0){
+        if(cashGiven >= billAmount){
+            showError('');
+            notefield.innerHTML = '<th>No. of notes</th>';
+            calculateNotes(cashGiven - billAmount)
+        }
+        else{
+            showError('Give me more cash.')
+        }
+    }
+    else{
+        showError('Invalid Amount')
+    }
+}
+
+checkBtn.addEventListener('click', validateAmounts)
+
+// notefield.innerHTML = '<th>No. of notes</th>';
